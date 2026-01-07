95e1a00fdb8868d5c35e7fe19cc1fc9795b3e83a
Kurmanbek-1 <kurmanbekmomunkulov32@gmail.com>
2026-01-07 15:30:35 +0600

    Lesson 3

--- main.py
+++ main.py
@@ -3,12 +3,30 @@
 import flet as ft 
 
 def main(page: ft.Page):
+    page.title = "Мое первое приложение!"
+    page.theme_mode = ft.ThemeMode.LIGHT
+
     text_hello = ft.Text(value='Hello world')
 
-    text_button = ft.TextButton('SEND')
-    elevated_button = ft.ElevatedButton('SEND')
+    def on_button_click(_):
+        name = name_input.value.strip()
+        print(name)
 
-    page.add(text_hello, text_button, elevated_button)
+        if name:
+            text_hello.color = None
+            text_hello.value = f"Hello {name}"
+            name_input.value = None
+        else:
+            text_hello.value = 'Введите корректное имя'
+            text_hello.color = ft.Colors.RED
+            
+        # page.update() 
+
+    elevated_button = ft.ElevatedButton('SEND', icon=ft.Icons.SEND, on_click=on_button_click)
+
+    name_input = ft.TextField(label='Введите имя', on_submit=on_button_click)
+
+    page.add(text_hello, name_input, elevated_button)
 
 
-ft.app(target=main, view=ft.AppView.WEB_BROWSER)
+ft.app(target=main)
